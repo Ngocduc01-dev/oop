adfcc0c1b53b719f5cf3b0304bc22f649a3760b0
Ngocduc01 <ducstd1@gmail.com>
2025-09-19 21:17:51 +0700

    Create Introduction.java
    Tạo file Introduction.java và làm theo yêu cầu tại phần B HW1

---
+++ HW1/Introduction.java
@@ -0,0 +1,17 @@
+public class Introduction {
+    public static void main(String[] args) {
+        String name = "Nguyễn Ngọc Đức";
+        String msv = "24020080";
+        String lop = "INT2204_4";
+        String username = "Ngocduc01-dev";
+        String mail = "24020080@vnu.edu.vn";
+        System.out.println(name + "\t" + msv + "\t" + lop +"\t" + username +"\t" + mail);
+        for (int i = 9 ;i>1 ;i-- ) {
+            System.out.println(i + " bottles of beer on the wall, "+ i + " bottles of beer.");
+            System.out.println("Take one down, pass it around,");
+        }
+        System.out.println("1 bottle of beer on the wall, 1 bottle of beer.");
+        System.out.println("Take one down, pass it around,");
+        System.out.println("No more bottles of beer on the wall.");
+    }
+}
