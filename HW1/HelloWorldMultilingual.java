da0a3d8d8a1fa961a1f2271fbd78ce98461d92d0
Ngocduc01 <ducstd1@gmail.com>
2025-09-20 10:04:08 +0700

    HW1 C
    Exercise HW1 : Part 1.1

---
+++ HW1/HelloWorldMultilingual.java
@@ -0,0 +1,15 @@
+public class HelloWorldMultilingual {
+    public static void main(String[] args) {
+        System.out.println("Hello, World"); //English
+        System.out.println("Xin chào thế giới"); //Vietnamese
+        System.out.println("你好世界"); //Chinese
+        System.out.println("Γεια σου, κόσμε"); //Greek
+        System.out.println("こんにちは世界"); //Japanese
+        System.out.println("안녕하세요, 세상"); //Korean
+        System.out.println("Привет, мир"); //Russia
+        System.out.println("Bonjour le monde"); //France
+        System.out.println("Hola Mundo"); //Spanish
+        System.out.println("Olá, Mundo"); //Portuguese
+        System.out.println("مرحبا بالعالم"); //Arab
+    }
+}
