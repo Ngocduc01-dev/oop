8f79f5976f3af337ca940af7d0cb5f01f2320504
Ngocduc01 <ducstd1@gmail.com>
2025-10-01 17:18:59 +0700

    Add TetrisGrid.java in HW2

--- HW2/src/main/java/TetrisGrid.java
+++ HW2/src/main/java/TetrisGrid.java
@@ -3,13 +3,14 @@
 // a clearRows() capability.
 
 public class TetrisGrid {
-	
+	private boolean [][]grid;
 	/**
 	 * Constructs a new instance with the given grid.
 	 * Does not make a copy.
 	 * @param grid
 	 */
 	public TetrisGrid(boolean[][] grid) {
+        this.grid = grid;
 	}
 	
 	
@@ -17,13 +18,56 @@ public class TetrisGrid {
 	 * Does row-clearing on the grid (see handout).
 	 */
 	public void clearRows() {
+       int width = grid.length;
+       int height = grid[0].length;
+
+       for (int y = 0; y < height; y++) {
+           if(isFullRow(y,width)) {
+               clearRow(y,width,height);
+               y--;
+           }
+       }
 	}
-	
+	private boolean isFullRow(int y, int width) {
+        for (int x = 0; x < width ; x++) {
+            if(!grid[x][y]) return false;
+        }
+        return true;
+    }
+
+    private void clearRow(int row, int width, int height) {
+        for (int y = row; y < height - 1; y++) {
+            for (int x = 0; x < width; x++) {
+                grid[x][y] = grid[x][y + 1];
+            }
+        }
+        for (int x = 0; x < width; x++) {
+            grid[x][height - 1] = false;
+        }
+    }
 	/**
 	 * Returns the internal 2d grid array.
 	 * @return 2d grid array
 	 */
 	boolean[][] getGrid() {
-		return null; // YOUR CODE HERE
+
+        return grid; // YOUR CODE HERE
 	}
+    public static void main(String[] args) {
+        boolean[][] grid = {
+                {false, false, true},
+                {true,  true,  true}
+        };
+        TetrisGrid tg = new TetrisGrid(grid);
+
+        tg.clearRows();
+
+        boolean[][] result = tg.getGrid();
+        for (int y = result[0].length - 1; y >= 0; y--) {
+            for (int x = 0; x < result.length; x++) {
+                System.out.print(result[x][y] ? "X " : ". ");
+            }
+            System.out.println();
+        }
+    }
 }
